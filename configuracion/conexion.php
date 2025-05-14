fef0cb2fe9d90498703f364e286948c18e6d4b2c
nath-696 <nathalymartinez696@gmail.com>
2025-05-14 11:36:37 -0600

    cambio

--- configuracion/conexion.php
+++ configuracion/conexion.php
@@ -5,7 +5,7 @@ $host = "localhost";
 $port = "5432";
 $dbname = "syscamp";
 $user = "postgres";
-$password = "1234s-"; // <-- cambia esto por la contraseña de tu usuario
+$password = "ivania0807"; // <-- cambia esto por la contraseña de tu usuario
 
 // Conexión nativa a PostgreSQL
 $conexion = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
