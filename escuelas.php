78ad5aeceef01c694fad66a201c07895623c6c66
Caleb <130073279+caleb-bvm@users.noreply.github.com>
2025-05-14 10:34:37 -0600

    espacios

--- escuelas.php
+++ escuelas.php
@@ -5,6 +5,7 @@ include_once('header.php');?>
 <div style="display: flex; justify-content: center;">
     <iframe src="https://www.google.com.qa/maps/d/u/0/embed?mid=179MI3LJPwQfyKNWvX4jRKWOoQY9rdo4&ehbc=2E312F" width="1400" height="800" style="max-width: 100%;"></iframe>
 </div>
+<br><br><br>
 
 <?php
 include_once('footer.php');?>
